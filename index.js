5883dbc7aee2febad0f836e2d66b1a0e59fb4aba
rajesh2024-krita <rajesh.kritatechnosolutions@gmail.com>
2025-12-26 13:52:42 +0530

    changes

--- index.js
+++ index.js
@@ -6,13 +6,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-// app.use(cors());
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
-
+app.use(cors());
 
 app.use(express.json({ limit: "10mb" }));
 
